71102c339ec60de9472963cf3f2a9cc70a8ccce8
jamalludin <jamalludin1209@gmail.com>
2021-11-01 17:01:42 +0700

    first init

---
+++ app/src/main/java/id/co/myabsensi/splashScreen/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package id.co.myabsensi.splashScreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import id.co.myabsensi.R
+import id.co.myabsensi.userAuth.UserLoginActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    lateinit var handler : Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, UserLoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/id/co/myabsensi/userAuth/UserLoginActivity.kt
@@ -0,0 +1,12 @@
+package id.co.myabsensi.userAuth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.myabsensi.R
+
+class UserLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_login)
+    }
+}
